e019ead61ce30eddae99eae81daafd81a0afd361
Jo Lynch <jo.k.lynch@gmail.com>
2023-03-08 08:54:23 +1100

    testing github sync

---
+++ Average3.java
@@ -0,0 +1,13 @@
+public class Average3
+{
+	public static void main(String[] args)
+	{
+		int num1 = 75;
+		int num2 = 137;
+
+		double average = (num1 + num2)/2;
+
+		System.out.print("The average of " + num1);
+		System.out.println(" and " + num2 + " is " + average);
+		}
+}
